363e39321d7da3aa18db3b88a0195bd04b99da78
seokheejang <shjang@deleo.co.kr>
2022-06-14 18:54:41 +0900

    declare moudle check at .d.ts. file

--- 01-typescript/background_v1/src/background.ts
+++ 01-typescript/background_v1/src/background.ts
@@ -6,14 +6,43 @@ export async function initialize() {
 };
 
 async function setupController() {
-    console.log("background: setupController");
+    try {
+        console.log("background: setupController");
+        
+        const password = '1234';
+        /**
+         * @TODO Promise type
+         */
+        const encryptor = {
+            encrypt(password: string, object: unknown): Promise<string> {
+                return new Promise('encrypted!');
+              },
+            decrypt(password: string, encryptedString: string) {
+                return new Promise({ foo: 'bar' });
+            },
+        }
 
-    const keyringObj = new KeyringController({
-        //keyringTypes: additionalKeyrings,
-        initState: {},
-        encryptor: undefined,
-    });
-    console.log("keyringObj", keyringObj)
+        const keyringObj: KeyringController = new KeyringController({
+            keyringTypes: '',
+            initState: {},
+            encryptor: undefined,
+        });
+        console.log("keyringObj", keyringObj);
+    
+        let vault: unknown;
+    
+        const accounts: string = await keyringObj.getAccounts();
+        console.log("account", accounts);
+        if (accounts.length > 0) {
+            vault = await keyringObj.fullUpdate();
+        } else {
+            vault = await keyringObj.createNewVaultAndKeychain(password);
+        }
+        
+        console.log("vault", vault);
+    } catch (e) {
+        console.log('setupController:', e);
+    }
 }
 
 initialize()
