c820bbfc432e419452966c4baa176a4e8164c85f
seokheejang <shjang@deleo.co.kr>
2022-07-18 17:01:39 +0900

    initial wallet extension example

---
+++ 02-blockchain/wallet/extension-example/popup.js
@@ -0,0 +1,9 @@
+let craeteWallet = document.getElementById("craeteWallet");
+let getWalletInfo = document.getElementById("getWalletInfo");
+
+craeteWallet.addEventListener("click", async () => {
+  console.log("craeteWallet");
+});
+getWalletInfo.addEventListener("click", async () => {
+  console.log("getWalletInfo");
+});
